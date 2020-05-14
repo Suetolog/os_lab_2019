b4aabc4f37c866ffad01eed26c6f010e492f452b
suetolog <artem.kriushov@mail.ru>
2020-05-14 12:46:52 +0000

    second task complite

---
+++ lab4/src/zombie.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<sys/types.h>  //ps -eaf
+
+int main()
+{
+    int i;
+    int pid = fork();
+    if (pid==0)
+    {
+        for (i=0; i<10; i++)
+            printf("Child\n");
+    }
+    else
+    {
+        printf("Parent\n");
+        while(1);
+    }
+}
+
